50f35d93eece2fe91292a0dde38fa209c981803f
Haris <muhdharxs@gmail.com>
2025-02-17 13:25:56 +0800

    Changes

--- hdb-price-prediction/prediction/predict.py
+++ hdb-price-prediction/prediction/predict.py
@@ -1,23 +1,28 @@
 from joblib import load
-import configparser
-from pathlib import Path
+import os
 import pandas as pd
 
-PROCESSED_DATA_PATH = "/app/data/processed_data.csv"
-PRED_DATA_PATH = "/app/data/pred_data.csv"
+# Load paths from environment variables set by the ConfigMap
+PROCESSED_DATA_PATH = os.getenv('PROCESSED_DATA_PATH')
+PRED_DATA_PATH = os.getenv('PRED_DATA_PATH')
+MODEL_SAVE_PATH = os.getenv('MODEL_SAVE_PATH')
+
+# Read data
 test = pd.read_csv(PRED_DATA_PATH)
 train = pd.read_csv(PROCESSED_DATA_PATH)
 
-model = load(f'data/saved_model.joblib')
+# Load the trained model
+model = load(f'{MODEL_SAVE_PATH}saved_model.joblib')
 
+# Ensure the test data has the same features as training data
 train_columns = train.drop(columns=['resale_price']).columns
 test = test[train_columns]
 
-
+# Make predictions
 predictions = model.predict(test)
-
 test['Predictions'] = predictions
 
-# Save to a new CSV file
-test.to_csv(f'/app/data/predictions_output.csv', index=False)
+# Save predictions
+test.to_csv(f'{MODEL_SAVE_PATH}predictions_output.csv', index=False)
 
+print("Predictions saved successfully at /app/data/predictions_output.csv")
